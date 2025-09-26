6e9135c16297dfd94766c3bc98afebb6e41e31fe
lewis-wow <ludvik.prokopec@gmail.com>
2025-09-26 13:08:26 +0200

    feat: env validation

---
+++ src/env.ts
@@ -0,0 +1,36 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    NODE_TLS_REJECT_UNAUTHORIZED: z.coerce.number().min(0).max(1).default(1),
+    AZURE_POD_IDENTITY_AUTHORITY_HOST: z.url(),
+    AZURE_KEY_VAULT_HOST: z.url(),
+  },
+
+  /**
+   * What object holds the environment variables at runtime. This is usually
+   * `process.env` or `import.meta.env`.
+   */
+  runtimeEnvStrict: {
+    NODE_TLS_REJECT_UNAUTHORIZED: process.env.NODE_TLS_REJECT_UNAUTHORIZED,
+    AZURE_POD_IDENTITY_AUTHORITY_HOST:
+      process.env.AZURE_POD_IDENTITY_AUTHORITY_HOST,
+    AZURE_KEY_VAULT_HOST: process.env.AZURE_KEY_VAULT_HOST,
+  },
+
+  /**
+   * By default, this library will feed the environment variables directly to
+   * the Zod validator.
+   *
+   * This means that if you have an empty string for a value that is supposed
+   * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
+   * it as a type mismatch violation. Additionally, if you have an empty string
+   * for a value that is supposed to be a string with a default value (e.g.
+   * `DOMAIN=` in an ".env" file), the default value will never be applied.
+   *
+   * In order to solve these issues, we recommend that all new projects
+   * explicitly specify this option as true.
+   */
+  emptyStringAsUndefined: true,
+});
