53ed9cc2e41b04e37a29800d11a4e519c5ae3534
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2025-09-26 13:08:45 +0200

    feat: env validation (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { DefaultAzureCredential } from '@azure/identity';
 import { KeyClient, KeyClientOptions } from '@azure/keyvault-keys';
+import { env } from './env.js';
 
 const credential = new DefaultAzureCredential();
-const url = 'https://localhost:3443';
 const options: KeyClientOptions = {
   serviceVersion: '7.4',
   // DEV ONLY
   disableChallengeResourceVerification: true,
 };
 
-const client = new KeyClient(url, credential, options);
+const client = new KeyClient(env.AZURE_KEY_VAULT_HOST, credential, options);
 const keyVaultKey = await client.createRsaKey('rsa-key', {
   keySize: 2048,
   keyOps: ['encrypt', 'decrypt', 'wrapKey', 'unwrapKey'],
