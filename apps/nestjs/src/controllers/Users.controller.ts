01119a92feb696ab02ffec373b8965269451b65e
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-02 19:24:46 +0200

    add type

--- apps/nestjs/src/controllers/Users.controller.ts
+++ apps/nestjs/src/controllers/Users.controller.ts
@@ -1,12 +1,12 @@
-
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import type { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get('profile')
   @UseGuards(AuthGuard('jwt'))
-  getProfile(@Req() req) {
+  getProfile(@Req() req: Request) {
     return req.user;
   }
 }
