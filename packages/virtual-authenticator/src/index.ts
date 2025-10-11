d1a9525c81596adf6c3bc45e1fc2acc73d00a9f3
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-11 15:38:50 +0200

    add barrels to enums

--- packages/virtual-authenticator/src/index.ts
+++ packages/virtual-authenticator/src/index.ts
@@ -1,6 +1,4 @@
 export * from './VirtualAuthenticator.js';
-export * from './types.js';
-export * from './enums/AuthenticatorAttachment.js';
 export * from './dto/AuthenticatorAssertionResponseDto.js';
 export * from './dto/AuthenticatorAttestationResponseDto.js';
 export * from './dto/PublicKeyCredentialDto.js';
