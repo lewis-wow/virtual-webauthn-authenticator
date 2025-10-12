130f7ab76ca88521cf9f7a237855f8fb48b0834a
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-12 12:40:25 +0200

    fix

--- packages/validation/src/credentials/AuthenticationExtensionsClientInputsSchema.ts
+++ packages/validation/src/credentials/AuthenticationExtensionsClientInputsSchema.ts
@@ -1,4 +1,7 @@
 import type { IAuthenticationExtensionsClientInputs } from '@repo/types';
 import z from 'zod';
 
-export const AuthenticationExtensionsClientInputsSchema = z.record(z.unknown()) satisfies z.ZodType<IAuthenticationExtensionsClientInputs>;
+export const AuthenticationExtensionsClientInputsSchema = z.record(
+  z.string(),
+  z.unknown(),
+) satisfies z.ZodType<IAuthenticationExtensionsClientInputs>;
