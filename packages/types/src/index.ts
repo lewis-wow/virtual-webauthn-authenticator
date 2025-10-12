8b03b8c30fed2f42c70a52dd16efa9e749afcabf
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-12 12:51:27 +0200

    fixes

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,9 +1,9 @@
 export * from './types/index.js';
 export * from './Base64URLString.js';
-
-import type { JsonWebKey } from 'node:crypto';
-
-export type MaybePromise<T> = T | Promise<T>;
+export * from './ICredentialPublicKey.js';
+export * from './ICredentialSigner.js';
+export * from './Jwk.js';
+export * from './MaybePromise.js';
 
 export type PropertyKey = string | number | symbol;
 
@@ -12,18 +12,3 @@ export type BufferLike = string | Buffer | ArrayBuffer | ArrayBufferView;
 export type SwapKeysAndValues<T extends Record<PropertyKey, PropertyKey>> = {
   [K in keyof T as T[K]]: K;
 };
-
-/**
- * The standard interface for a JSON Web Key.
- */
-export interface Jwk extends JsonWebKey {
-  alg?: string;
-}
-
-export interface ICredentialPublicKey {
-  getJwk: () => MaybePromise<Jwk>;
-}
-
-export interface ICredentialSigner {
-  sign: (data: Buffer) => MaybePromise<Buffer>;
-}
