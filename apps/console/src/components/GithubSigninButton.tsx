ba0fa3b38b6ef7902423d8d345f4882fcc629429
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-13 11:04:21 +0200

    feat: github auth

--- apps/console/src/components/GithubSigninButton.tsx
+++ apps/console/src/components/GithubSigninButton.tsx
@@ -1,19 +1,24 @@
 'use client';
 
 import { Button } from '@/components/Button';
-import { FaGoogle } from 'react-icons/fa';
+import { signIn } from '@/lib/auth';
+import { FaGithub } from 'react-icons/fa';
 
 export const GoogleSigninButton = () => {
-  const handleGoogleSignIn = async () => {};
+  const handleSignIn = async () => {
+    await signIn.social({
+      provider: 'github',
+    });
+  };
 
   return (
     <Button
-      onClick={handleGoogleSignIn}
+      onClick={handleSignIn}
       className="flex w-full items-center justify-center gap-3 border border-gray-300 bg-white text-gray-900 hover:bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
       size="lg"
     >
-      <FaGoogle className="h-5 w-5" />
-      Pokračovat přes Google
+      <FaGithub className="h-5 w-5" />
+      Sign in with Github
     </Button>
   );
 };
--- packages/better-auth/src/auth.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '@repo/prisma';
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
-
-export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: 'postgresql',
-  }),
-});
