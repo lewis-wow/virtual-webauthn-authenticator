dd83dcc51c2e5c99c37d272a4c93057bac1077ba
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-13 15:00:33 +0200

    fix

--- apps/crxjs/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*?script&module' {
-  const src: string;
-  export default src;
-}
--- apps/crxjs/vite.config.ts
+++ apps/crxjs/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import zip from 'vite-plugin-zip-pack';
 
 import manifest from './manifest.config.js';
-import { name, version } from './package.json';
 
 export default defineConfig({
   resolve: {
@@ -14,7 +13,7 @@ export default defineConfig({
   },
   plugins: [
     crx({ manifest }),
-    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
+    zip({ outDir: 'release', outFileName: 'release.zip' }),
   ],
   server: {
     cors: {
