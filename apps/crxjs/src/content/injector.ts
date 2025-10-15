1c9f515eb1f5984acf16b23790cec5ef413725e7
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-15 20:54:49 +0200

    remove crxjs

--- apps/crxjs/src/content/injector.ts
+++
@@ -1,8 +0,0 @@
-import overrideUrl from './override.js?url';
-
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL(overrideUrl);
-script.dataset.apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-
-script.onload = () => script.remove();
-(document.head || document.documentElement).appendChild(script);
