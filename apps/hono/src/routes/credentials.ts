6d8a98c7204d7d14891195fef85c14800d8e3903
Ludvík Prokopec <39367469+lewis-wow@users.noreply.github.com>
2025-10-17 13:28:38 +0200

    chore: remove elysia (#32)

---
+++ apps/hono/src/routes/credentials.ts
@@ -0,0 +1,38 @@
+import { zValidator } from '@hono/zod-validator';
+import type {
+  IAuthenticatorAssertionResponse,
+  IAuthenticatorAttestationResponse,
+} from '@repo/types';
+import {
+  AuthenticatorAssertionResponseSchema,
+  AuthenticatorAttestationResponseSchema,
+  PublicKeyCredentialCreationOptionsSchema,
+  PublicKeyCredentialRequestOptionsSchema,
+} from '@repo/validation';
+import { Hono } from 'hono';
+
+import { zResponseValidator } from '../middleware/zResponseValidator';
+
+export const credentials = new Hono().basePath('/credentials');
+
+credentials.get(
+  '/',
+  zValidator('query', PublicKeyCredentialRequestOptionsSchema),
+  zResponseValidator(AuthenticatorAssertionResponseSchema),
+  async (ctx) => {
+    const publicKeyCredentialRequestOptions = ctx.req.valid('query');
+
+    return ctx.json<IAuthenticatorAssertionResponse>({} as any);
+  },
+);
+
+credentials.post(
+  '/',
+  zValidator('json', PublicKeyCredentialCreationOptionsSchema),
+  zResponseValidator(AuthenticatorAttestationResponseSchema),
+  async (ctx) => {
+    const publicKeyCredentialCreationOptions = ctx.req.valid('json');
+
+    return ctx.json<IAuthenticatorAttestationResponse>({} as any);
+  },
+);
