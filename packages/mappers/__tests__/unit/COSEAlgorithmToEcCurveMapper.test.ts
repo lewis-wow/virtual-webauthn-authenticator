c0479ca95fece397445af6e2fc0f1aae2f6491a8
lewis-wow <ludvik.prokopec@gmail.com>
2025-10-21 14:36:33 +0200

    fix tests

--- packages/mappers/__tests__/unit/COSEAlgorithmToEcCurveMapper.test.ts
+++ packages/mappers/__tests__/unit/COSEAlgorithmToEcCurveMapper.test.ts
@@ -1,29 +1,29 @@
-import { COSEAlgorithm, EcCurve } from '@repo/enums';
+import { COSEKeyAlgorithm, KeyCurveName } from '@repo/enums';
 import { describe, it, expect } from 'vitest';
 
-import { COSEAlgorithmToEcCurveMapper } from '../../src/COSEAlgorithmToEcCurveMapper';
+import { COSEAlgorithmToKeyCurveNameMapper } from '../../src/COSEAlgorithmToEcCurveMapper';
 
 describe('COSEAlgorithmToEcCurve', () => {
   it('should map ES256 to P256', () => {
-    expect(COSEAlgorithmToEcCurveMapper(COSEAlgorithm.ES256)).toBe(
-      EcCurve.P256,
+    expect(COSEAlgorithmToKeyCurveNameMapper(COSEKeyAlgorithm.ES256)).toBe(
+      KeyCurveName.P256,
     );
   });
 
   it('should map ES384 to P384', () => {
-    expect(COSEAlgorithmToEcCurveMapper(COSEAlgorithm.ES384)).toBe(
-      EcCurve.P384,
+    expect(COSEAlgorithmToKeyCurveNameMapper(COSEKeyAlgorithm.ES384)).toBe(
+      KeyCurveName.P384,
     );
   });
 
   it('should map ES512 to P521', () => {
-    expect(COSEAlgorithmToEcCurveMapper(COSEAlgorithm.ES512)).toBe(
-      EcCurve.P521,
+    expect(COSEAlgorithmToKeyCurveNameMapper(COSEKeyAlgorithm.ES512)).toBe(
+      KeyCurveName.P521,
     );
   });
 
   it('should throw an error for an unknown COSE algorithm', () => {
-    const unknownAlgorithm = 999 as COSEAlgorithm;
-    expect(() => COSEAlgorithmToEcCurveMapper(unknownAlgorithm)).toThrow();
+    const unknownAlgorithm = 999 as COSEKeyAlgorithm;
+    expect(() => COSEAlgorithmToKeyCurveNameMapper(unknownAlgorithm)).toThrow();
   });
 });
