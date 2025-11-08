9b52bdec7f9a02fd12e398219d1b75779d900b13
lewis-wow <ludvik.prokopec@gmail.com>
2025-11-08 20:22:54 +0100

    fixes

--- apps/console/src/types.ts
+++ apps/console/src/types.ts
@@ -2,7 +2,8 @@ import type { ReactNode } from 'react';
 import type { FieldValues, Path, UseFormReturn } from 'react-hook-form';
 
 export type CommonFieldProps<TFieldValues extends FieldValues> = {
-  form: UseFormReturn<TFieldValues, unknown, FieldValues>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  form: UseFormReturn<TFieldValues, any, any>;
   name: Path<TFieldValues>;
   label?: ReactNode;
   hint?: ReactNode;
