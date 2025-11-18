a1c318d825ac8804de0935137b75d9ff0c291781
lewis-wow <ludvik.prokopec@gmail.com>
2025-11-18 14:03:05 +0100

    move enums

--- packages/virtual-authenticator/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './VirtualAuthenticator';
-
-export * from './enums/SupportedCOSEKeyAlgorithm';
-export * from './enums/SupportedPublicKeyCredentialType';
-
-export * from './exceptions/AttestationNotSupported';
-export * from './exceptions/CredentialNotFound';
-export * from './exceptions/NoSupportedPubKeyCredParamWasFound';
