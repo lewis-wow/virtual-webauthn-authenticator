86046e0e13158c25c06059afaba7189a42efe802
lewis-wow <ludvik.prokopec@gmail.com>
2025-11-24 15:20:55 +0100

    fix

---
+++ packages/virtual-authenticator/src/zod-validation/WebAuthnCredentialBaseSchema.ts
@@ -0,0 +1,21 @@
+import z from 'zod';
+
+import { BytesSchema } from './BytesSchema';
+
+export const WebAuthnCredentialBaseSchema = z
+  .object({
+    id: z.uuid(),
+    name: z.string().nullable(),
+    userId: z.string(),
+    COSEPublicKey: BytesSchema,
+    counter: z.number().int().nonnegative(),
+    transports: z.array(z.string()),
+    rpId: z.string(),
+  })
+  .meta({
+    id: 'WebAuthnCredential',
+  });
+
+export type WebAuthnCredentialBase = z.infer<
+  typeof WebAuthnCredentialBaseSchema
+>;
