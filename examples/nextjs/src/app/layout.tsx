61af1272c42c1e89d1b519ac80b0434f907ad41a
lewis-wow <ludvik.prokopec@gmail.com>
2025-12-07 21:41:22 +0100

    feat: example

---
+++ examples/nextjs/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Providers } from '@/Providers';
+import { Toaster } from '@repo/ui/components/ui/sonner';
+import '@repo/ui/globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: process.env.NEXT_PUBLIC_APP_NAME,
+  description: 'Virtual WebAuthn Authenticator',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className="h-full bg-background font-sans antialiased">
+        <main className="h-full">
+          <Providers>{children}</Providers>
+        </main>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
