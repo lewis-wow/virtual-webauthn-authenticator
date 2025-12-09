e62c3faecfbfb3cac57858064c54e0940d5871b3
lewis-wow <ludvik.prokopec@gmail.com>
2025-12-09 21:17:24 +0100

    fixes

--- apps/wxt/env.ts
+++ apps/wxt/env.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 export const env = defineEnv({
   clientPrefix: 'WXT_',
   client: {
+    WXT_APP_NAME: z.string(),
     WXT_API_BASE_URL: z.url(),
   },
   runtimeEnv: import.meta.env,
