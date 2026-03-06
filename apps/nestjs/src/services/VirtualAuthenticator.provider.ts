997cfa585747fbe720f30ec41fbd2bd13f16092f
lewis-wow <ludvik.prokopec@gmail.com>
2026-03-06 13:56:11 +0100

    fix

--- apps/nestjs/src/services/VirtualAuthenticator.provider.ts
+++ apps/nestjs/src/services/VirtualAuthenticator.provider.ts
@@ -2,6 +2,13 @@ import { Provider } from '@nestjs/common';
 import { AzureKeyVaultKeyProvider } from '@repo/key-vault';
 import { VirtualAuthenticator } from '@repo/virtual-authenticator';
 import {
+  AttestationHandlerRegistry,
+  AttestationProcessor,
+  AuthorizationGesture,
+  NoneAttestationHandler,
+  PackedAttestationHandler,
+} from '@repo/virtual-authenticator/authenticator';
+import {
   PrismaVirtualAuthenticatorRepository,
   PrismaWebAuthnRepository,
 } from '@repo/virtual-authenticator/repositories';
@@ -13,10 +20,25 @@ export const VirtualAuthenticatorProvider: Provider = {
     azureKeyVaultKeyProvider: AzureKeyVaultKeyProvider,
     virtualAuthenticatorRepository: PrismaVirtualAuthenticatorRepository,
   ) => {
+    const authorizationGesture = new AuthorizationGesture({
+      virtualAuthenticatorRepository,
+    });
+
+    const attestationHandlerRegistry =
+      new AttestationHandlerRegistry().registerAll([
+        new NoneAttestationHandler(),
+        new PackedAttestationHandler({ keyProvider: azureKeyVaultKeyProvider }),
+      ]);
+    const attestationProcessor = new AttestationProcessor(
+      attestationHandlerRegistry,
+    );
+
     const virtualAuthenticator = new VirtualAuthenticator({
       webAuthnRepository,
       keyProvider: azureKeyVaultKeyProvider,
       virtualAuthenticatorRepository,
+      authorizationGesture,
+      attestationProcessor,
     });
 
     return virtualAuthenticator;
