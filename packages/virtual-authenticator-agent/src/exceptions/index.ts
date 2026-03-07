c2ba6d58302f0d54e6d5ad09841b809ebf995ee5
lewis-wow <ludvik.prokopec@gmail.com>
2026-03-07 21:40:07 +0100

    fix

--- packages/virtual-authenticator-agent/src/exceptions/index.ts
+++ packages/virtual-authenticator-agent/src/exceptions/index.ts
@@ -1,4 +1,10 @@
-export * from './CreateCredentialActionNotDefined';
-export * from './CredentialSelectAgentException';
-export * from './UserPresenceRequiredAgentException';
-export * from './UserVerificationRequiredAgentException';
+/**
+ * @file Automatically generated.
+ *
+ * @noprettier
+ */
+
+export * from "./CreateCredentialActionNotDefined";
+export * from "./CredentialSelectAgentException";
+export * from "./UserPresenceRequiredAgentException";
+export * from "./UserVerificationRequiredAgentException";
